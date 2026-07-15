c867fdda5f87c277fe1dadcec68310245b744da8
Robin Lieb <34332703+robinlieb@users.noreply.github.com>
2026-07-15 21:31:07 +0200

    chore: update go dependencies
    Signed-off-by: Robin Lieb <34332703+robinlieb@users.noreply.github.com>

--- api/v1alpha1/menu_types.go
+++ api/v1alpha1/menu_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // OverridePolicyType controls whether consumers may override values or patches.
@@ -184,5 +185,8 @@ type MenuList struct {
 }
 
 func init() {
-	SchemeBuilder.Register(&Menu{}, &MenuList{})
+	SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(SchemeGroupVersion, &Menu{}, &MenuList{})
+		return nil
+	})
 }
